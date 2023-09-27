5bbdb4acb624e174f8ed35f81465f305cc028d38
przybylski.pawel <przybylski.pawel@STACJA105-06>
2023-09-27 14:09:03 +0200

    Dodaj pliki projektów.

---
+++ Deklar/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+using System;
+using System.IO;
+
+
+
+namespace Popup
+{
+    internal class MójPies
+    {
+        public string ZbierzDane(string imie, string nazwisko, string nazwaPsa, DateTime dataUrodzenia)
+        {
+            return $"Imię właściciela: {imie}\n" +
+                   $"Nazwisko właściciela: {nazwisko}\n" +
+                   $"Nazwa psa: {nazwaPsa}\n" +
+                   $"Data urodzenia: {dataUrodzenia.ToShortDateString()}\n" +
+                   $"Rodowod: {nazwaPsa}\n";
+        }
+
+
+        public void ZapiszDane(string dane)
+        {
+            File.WriteAllText("dane.txt", dane);
+        }
+    }
+}
---
+++ Deklar/MójPies.cs
@@ -0,0 +1,74 @@
+﻿
+
+using Popup;
+using System;
+
+using System.ComponentModel;
+
+using System.Windows;
+
+
+
+namespace Deklar
+
+{
+
+    public partial class MainWindow : Window
+
+    {
+
+        public MainWindow()
+
+        {
+
+            InitializeComponent();
+
+            DataContext = new MyDog();
+
+        }
+
+
+
+        private void BtnWyslij_Click(object sender, RoutedEventArgs e)
+
+        {
+
+            MyDog dog = DataContext as MyDog;
+
+            MójPies pies = new MójPies();
+
+            string dane = pies.ZbierzDane(txtImieWlasciciela.Text, txtNazwiskoWlasciciela.Text, txtNazwaPsa.Text, dog.DtB);
+
+            pies.ZapiszDane(dane);
+
+            MessageBox.Show("Dane zapisane!");
+
+        }
+
+    }
+
+
+
+  internal class MyDog:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void checkPropertyChangeEvent(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private DateTime dtB;
+            public DateTime DtB
+        {
+            get { return dtB; }
+            set { dtB = value;
+                checkPropertyChangeEvent("DtB");
+            }
+        }
+    }
+
+}
